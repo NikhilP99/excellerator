310fb7cdb64afbaac30aa83cf29b1705f718a73b
Nikhil Patel <nikhilpatel.indore@gmail.com>
2022-07-20 12:33:33 +0530

    Init

---
+++ src/main/java/me/np99/excellerator/ExcelleratorApplication.java
@@ -0,0 +1,13 @@
+package me.np99.excellerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelleratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelleratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/np99/excellerator/ExcelleratorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.np99.excellerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelleratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
